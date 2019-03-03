fa1745615622ce7bef165c6331fbebd671e0ba1f
beautyyuyanli <beautyyuyanli@outlook.com>
2019-03-03 11:46:44 +0800

    write the main board

--- scrips/gameBoard.js
+++ scrips/gameBoard.js
@@ -1,4 +1,5 @@
-var inputElement = document.getElementById("inputWord"), alarmElement, scoreElement, transElement = document.getElementById("showTrans");
+var gameBoard = document.getElementById("coreGame"), inputElement = document.getElementById("inputWord"), alarmElement, scoreElement, 
+    transElement = document.getElementById("showTrans"), overElement = document.getElementById("gameOver"), totleScoreElement = document.getElementById("showScore");
 function submitEvent(e){
     let keynum;
     if (window.event) // IE
@@ -10,25 +11,33 @@ function submitEvent(e){
         inputElement.value = "";
     }
 }
-
 function showTrans(_word){
     console.log(_word);
     transElement.innerHTML = _word;
 }
 function showGameOver(){
     console.log("game over" + score);
-
+    overElement.hidden = false;
+    totleScoreElement.innerHTML = "your score this time: " + score;
 }
 function showScore(_num){
     console.log("score" + _num);
-
 }
 function showAlarm(){
 
 }
-function gameBoardCreate(){
-
+function gameBoardCreate(maxWords, dieTime){
+    inputElement.value = "";
+    transElement.innerHTML = "";
+    overElement.hidden = true;
+    gameBoard.hidden = false;
+    let al = setInterval(() => {gameStart(maxWords, dieTime);clearInterval(al)}, 1000);
 }
 function gameBoardDelete(){
-
+    gameBoard.hidden = true;
+}
+function returnButton(){
+    // if...
+    gameBoardDelete();
+    mainBoardCreate(score);
 }
--- scrips/mainBoard.js
+++ scrips/mainBoard.js
@@ -1,11 +1,17 @@
-var historyScore, lastScore, exValue;
-var historyScoreElement, lastScoreElement, exElement;
-function play(){
-
+var historyScore = 0, lastScore = 0, exValue = 0;
+var historyScoreElement = document.getElementById("showHistoryScore"), lastScoreElement = document.getElementById("showLastScore"), 
+    exElement, mainBoard = document.getElementById("mainBoard");
+function playButton(){
+    mainBoardDelete();
+    gameBoardCreate(5, 10);
 }
-function mainBoardCreate(){
-
+function mainBoardCreate(_num = lastScore){
+    lastScore = _num;
+    historyScore = Math.max(historyScore, lastScore);
+    historyScoreElement.innerHTML = "the max score in history: " + historyScore;
+    lastScoreElement.innerHTML = "the score last game: " + lastScore;
+    mainBoard.hidden = false;
 }
 function mainBoardDelete(){
-
+    mainBoard.hidden = true;
 }
